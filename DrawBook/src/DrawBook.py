c42679306200cfd19f0284e4ea792a6f85e8bd63
daniel.waxweiler <daniel.waxweiler@ba35e0a5-d65c-67f0-5c3e-c6f1f0da9a22>
2011-07-01 20:03:03 +0000

    began with method draw

--- DrawBook/src/DrawBook.py
+++ DrawBook/src/DrawBook.py
@@ -16,28 +16,34 @@ import os
 class DrawBook(AVGApp):
   
   
-  def __init__(self, width, height, imageFolder, configFileName):
+  def __init__(self, width, height, folder):
     '''
     initializes the setting for the draw book
-    arguments: width, height (screen resolution to run in full screen mode), path
+    arguments: width, height (screen resolution to run in full screen mode), relative path
     '''
     self.height = height # height of the screen
     self.width = width # width of the screen
-    self.imageFolder = imageFolder # path to the folder which contains the images
+    self.folder = folder # path to the folder which contains the images
     self.counter = 0 # number of drawn images
     self.player = avg.Player.get() # libavg player
-    self.configFileName = configFileName # file name of the DrawBook configuration 
+    self.configFileName = 'drawbook_config.txt' # file name of the DrawBook configuration 
     self.player.loadString("""<avg size="("""+str(self.width)+""","""+str(self.height)+""")"></avg>""")
     self.player.setResolution(True, self.width, self.height, 32)
     self.masterDivNode = avg.DivNode(parent=self.player.getRootNode()) # div node which contains all the images
+    self.configuration = []; # list containing the configuration of the scene
     
-    # check if folder of images exists
-    if not os.path.isdir(self.imageFolder):
+    # check if folder exists
+    if not os.path.isdir(self.folder):
       exit('Folder containing the images does not exist!')
     
-    # create folder for resolution if it does not exist
-    if not os.path.isdir(self.imageFolder + "/" + str(self.width) + "x" + str(self.height)):
+    # create subfolder for current resolution if it does not exist
+    if not os.path.isdir(self.folder + "/" + str(self.width) + "x" + str(self.height)):
       os.makedirs(str(self.width) + "x" + str(self.height))
+      
+    # create configuration file if it does not exist
+    if not os.path.isfile(self.folder):
+      fopj = open(self.configFileName, "w")
+      fopj.close()
   
   
   def save(self):
@@ -58,19 +64,23 @@ class DrawBook(AVGApp):
     '''
     draws the images of the list
     '''
-    return 0
-    ''' delete this if not helpful
-    y = -self.height/10
-    for i in range(6):
-      x = -self.width/10
-      for j in range(6):
-        avg.RectNode(fillcolor="FFFFFF", fillopacity=1.0, parent=self.player.getRootNode(), 
-                     pos=(x, y), size=(self.width/5, self.height/5), strokewidth=0)
-        avg.WordsNode(color="000000", fontsize=25, parent=self.player.getRootNode(),
-                      pos=(x+50, y+self.height/10-25), text="Touch to draw here")
+    # calculate y coordinate of first image
+    y = (len(self.configuration)*(self.height/5+2)-2-self.height)/-2
+    for i in range(len(self.configuration)):
+      # calculate x coordinate of first image in each row
+      x = (len(self.configuration[i])*(self.width/5+2)-2-self.width)/-2
+      for j in range(len(self.configuration[i])):
+        path = self.folder + "/" + str(self.width) + "x" + str(self.height) + "/" + str(self.configuration[i][j]) + ".jpg"
+        if self.configuration[i][j] != 0 and os.path.isfile(path):
+          # draw image if it exists
+          avg.ImageNode(id=str(self.configuration[i][j]), href=path, parent=self.masterDivNode,
+                        pos=(x, y), size=(self.width/5, self.height/5))
+        else:
+          # draw rectangle if there is a "0" or image does not exist
+          avg.RectNode(id=str(i)+"x"+str(j), fillcolor="FFFFFF", fillopacity=1.0, parent=self.masterDivNode, 
+                       pos=(x, y), size=(self.width/5, self.height/5), strokewidth=0)
         x += self.width/5+2
       y += self.height/5+2
-    '''
   
   
   def counterUp(self):
@@ -90,6 +100,7 @@ class DrawBook(AVGApp):
     '''
     starts the draw book
     '''
+    self.draw()
     self.player.play()
   
   
@@ -102,7 +113,7 @@ class DrawBook(AVGApp):
 
 
 def main():
-  book = DrawBook(1440, 900, './', '')
+  book = DrawBook(1440, 900, './')
   book.start()
 
 if __name__ == '__main__':
