4900a630109f0d80141d0d259393ca84a1261167
daniel.waxweiler <daniel.waxweiler@ba35e0a5-d65c-67f0-5c3e-c6f1f0da9a22>
2011-07-21 22:19:09 +0000

    added chooser icon

--- DrawBook/src/Draw.py
+++ DrawBook/src/Draw.py
@@ -5,13 +5,13 @@ To do:
 
 Bugs:
 - strange effects when user is drawing and moves with the finger pressed down right over the tool bar
-- webcam maby not working; please test
+- webcam may be not working; please test
 '''
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from libavg import avg
+from libavg import avg, LinearAnim
 import math
 
 
@@ -23,26 +23,27 @@ class Draw(object):
     '''
     loads the tool bar and the draw surface
     '''
-    self.color  = "000000" # for pencil color
-    self.colorBarUnlink = True  # variable to prevent the bar to build twice
-    self.sizeBarUnlink = True   # variable to prevent the bar to build twice
+    self.color  = "000000"            # draw color
+    self.oldColor = self.color        # previous draw color
+    self.colorBarUnlink = True        # variable to prevent the bar to build twice
+    self.sizeBarUnlink = True         # variable to prevent the bar to build twice
     self.webcamUnlink = True    
-    self.pencilSize = 10 # radius of the pencil
-    self.j = j # y-position of the new drawing in the matrix
-    self.i = i # x-position of the new drawing in the matrix
-    self.player = player # libavg player
-    self.imageWidth = imageWidth # width of the new image
-    self.screenWidth = screenWidth # width of the screen
+    self.size = 10                    # size of the pencil or eraser (radius)
+    self.j = j                        # y-position of the new drawing in the matrix
+    self.i = i                        # x-position of the new drawing in the matrix
+    self.player = player              # libavg player
+    self.imageWidth = imageWidth      # width of the new image
+    self.screenWidth = screenWidth    # width of the screen
     self.screenHeight = screenHeight
-    self.folder = folder # folder where the new drawing should be stored
-    self.drawBook = drawBook # instance of the draw book
-    self.cursorIDs = {} # dictionary of all the touches in use with their last position
+    self.folder = folder              # folder where the new drawing should be stored
+    self.drawBook = drawBook          # instance of the draw book
+    self.cursorIDs = {}               # dictionary of all the touches in use with their last position
     
-    # create a container for all the tool bar elements
+    # create a container for all the toolbar elements
     self.toolBar = avg.DivNode(id="tools", parent=player.getRootNode())
-    # create the background of the tool bar
-    self.toolBarBackground = avg.RectNode(fillcolor="2a2a2a", fillopacity=1.0, 
-      parent=self.toolBar, pos=(0, 0),size=(screenWidth-imageWidth, screenHeight), strokewidth=0)
+    # create the background of the toolbar
+    self.toolBarBackground = avg.RectNode(fillcolor="2a2a2a", fillopacity=1.0, parent=self.toolBar, pos=(0, 0),
+                                          size=(screenWidth-imageWidth, screenHeight), strokewidth=0)
     # container that holds all the icons
     icons = avg.DivNode(id="icons", parent=self.toolBar, pos=(5, 5))
     self.icons = icons
@@ -55,45 +56,49 @@ class Draw(object):
     tool = avg.ImageNode(href="img/pencil.png", parent=icons, pos=(0, self.n), size=(self.n, self.n))
     self.tool = tool # for dynamic use of the pencil button position
     tool.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.pencil)
+    # color & size chooser
+    self.chooserNode = avg.CircleNode(fillcolor=self.color, fillopacity=1.0, parent=icons, pos=(self.n/2, 2*self.n+self.n/2),
+                                      r=self.size, strokewidth=3)
+    self.chooserNode.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.chooser)
     # eraser button
-    eraser = avg.ImageNode(href="img/eraser.png", parent=icons, pos=(0, 2*self.n), size=(self.n, self.n))
+    eraser = avg.ImageNode(href="img/eraser.png", parent=icons, pos=(0, 3*self.n), size=(self.n, self.n))
     eraser.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.eraser)
     self.canelSaveButton()
     # center the icons vertically
-    icons.y = (self.screenHeight - 5*self.n) / 2
+    icons.y = (self.screenHeight - 6*self.n) / 2
     
     # create canvas for the drawing surface
     self.drawCanvas = player.loadCanvasString("<canvas id=\"drawing\" width=\""+str(imageWidth)+"\" height=\""+str(screenHeight)+"\"></canvas>")
     avg.RectNode(fillcolor="FFFFFF", fillopacity=1.0, parent=self.drawCanvas.getRootNode(),
                  pos=(0, 0), size=(imageWidth, screenHeight), strokewidth=0)
-    self.creatDrawingSurface()
+    self.createDrawingSurface()
 
 
   def canelSaveButton(self):
     
     if self.webcamUnlink == False:
       # save button
-      save = avg.ImageNode(href="img/applycam.png", parent=self.icons, pos=(0, 3*self.n), size=(self.n, self.n))
+      save = avg.ImageNode(href="img/applycam.png", parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n))
       save.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.save)
       # cancel button
-      cancel = avg.ImageNode(href="img/cancelcam.png", parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n))
+      cancel = avg.ImageNode(href="img/cancelcam.png", parent=self.icons, pos=(0, 5*self.n), size=(self.n, self.n))
       cancel.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.cancel)
     else:
       # save button
       avg.RectNode(fillcolor=self.toolBarBackground.fillcolor, fillopacity=1.0,
-      parent=self.icons, pos=(0, 3*self.n), size=(self.n, self.n), strokewidth=0)
+      parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n), strokewidth=0)
       
-      save = avg.ImageNode(href="img/apply.png", parent=self.icons, pos=(0, 3*self.n), size=(self.n, self.n))
+      save = avg.ImageNode(href="img/apply.png", parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n))
       save.setEventHandler(avg.CURSORUP, avg.TOUCH|avg.MOUSE, self.save)
       # cancel button
       avg.RectNode(fillcolor=self.toolBarBackground.fillcolor, fillopacity=1.0,
-      parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n), strokewidth=0)
+      parent=self.icons, pos=(0, 5*self.n), size=(self.n, self.n), strokewidth=0)
       
-      cancel = avg.ImageNode(href="img/cancel.png", parent=self.icons, pos=(0, 4*self.n), size=(self.n, self.n))
+      cancel = avg.ImageNode(href="img/cancel.png", parent=self.icons, pos=(0, 5*self.n), size=(self.n, self.n))
       cancel.setEventHandler(avg.CURSORUP, avg.TOUCH|avg.MOUSE, self.cancel)
 
 
-  def creatDrawingSurface(self):    
+  def createDrawingSurface(self):    
     # load canvas in the scene
     self.drawingSurface = avg.ImageNode(id="surface", href="canvas:drawing", parent=self.player.getRootNode(),
                                  pos=(self.screenWidth-self.imageWidth, 0), size=(self.imageWidth, self.screenHeight))
@@ -128,6 +133,7 @@ class Draw(object):
       self.draw(self.cursorIDs[event.cursorid], event.pos)
       del self.cursorIDs[event.cursorid]
   
+  
   def draw(self, pos1, pos2):
     '''
     decides what to draw
@@ -149,7 +155,7 @@ class Draw(object):
     draws a circle node on the given position
     arguments: x, y: position
     '''
-    avg.CircleNode(fillcolor=self.color, fillopacity=1.0, parent=self.drawCanvas.getRootNode(), pos=(x, y), r=self.pencilSize, strokewidth=0)
+    avg.CircleNode(fillcolor=self.color, fillopacity=1.0, parent=self.drawCanvas.getRootNode(), pos=(x, y), r=self.size, strokewidth=0)
   
   
   def drawLineNode(self, x1, y1, x2, y2):
@@ -157,14 +163,14 @@ class Draw(object):
     draws a line node between the two given positions
     arguments: x1, y1: start position; x2, y2: end position
     '''
-    avg.LineNode(color=self.color, parent=self.drawCanvas.getRootNode(), pos1=(x1, y1), pos2=(x2, y2), strokewidth=self.pencilSize*2)
+    avg.LineNode(color=self.color, parent=self.drawCanvas.getRootNode(), pos1=(x1, y1), pos2=(x2, y2), strokewidth=self.size*2)
   
   
   def webcam(self, event):
     '''
     event handler function that starts the webcam and loads the image to the screen
-     # http://www.libavg.de/reference/current/areanodes.html#libavg.avg.CameraNode
-    '''  
+    http://www.libavg.de/reference/current/areanodes.html#libavg.avg.CameraNode
+    '''
     if self.webcamUnlink == True:
       self.camara = avg.CameraNode( id="camara", parent=self.player.getRootNode(), 
         pos=(self.screenWidth - self.imageWidth,0), size=(self.imageWidth, self.screenHeight) , 
@@ -185,7 +191,7 @@ class Draw(object):
       pos=(self.screenWidth - self.imageWidth,0), 
       size=(self.imageWidth, self.screenHeight), strokewidth=0)
     self.drawingSurface.unlink() #i think the draw surface have to be rebuild so u can draw on the new picture
-    self.creatDrawingSurface()
+    self.createDrawingSurface()
     self.camara.unlink()
     self.webcamUnlink = True
     self.canelSaveButton( )
@@ -202,19 +208,27 @@ class Draw(object):
     '''
     event handler function that selects pencil tool
     '''
-    self.erase = False
-    self.colorChoose()  # could be outsourced to another icon
-    self.sizeChoose()   # could be outsourced to another icon
+    self.color = self.oldColor
+  
+  
+  def chooser(self, event):
+    '''
+    event handler function that lets choose color and size
+    '''
+    self.colorChoose()
+    self.sizeChoose()
     
     
   def sizeChoose(self):  
-    ''' sets the available sizes and builds a container for them '''
+    '''
+    sets the available sizes and builds a container for them
+    '''
     sizeCircleArray = [5, 10, 15, 20, 25, 30, 35, 40]
     if self.sizeBarUnlink == True : # only build new sizeBar if previous was unlinked
       self.sizeBar = avg.DivNode( id="size", parent=self.player.getRootNode() ) # container for sizes
       self.sizeBarUnlink = False
 
-    ''' fill the sizeBar with the given sizes '''
+    # fill the sizeBar with the given sizes
     countWidth = 0
     count = 0
     
@@ -229,22 +243,26 @@ class Draw(object):
     
     
   def setSize (self, event):
-    self.pencilSize = event.node.r
+    self.size = event.node.r
     self.sizeBar.unlink( )
     self.sizeBarUnlink = True
+    self.updateChooserNode()
     
     self.colorBar.unlink( )     # have to be deleted if outsourced to another icon
     self.colorBarUnlink = True  # have to be deleted if outsourced to another icon
-    
+  
+  
   def colorChoose(self):  
-    ''' sets the available colors and builds a container for them '''
+    '''
+    sets the available colors and builds a container for them
+    '''
     # color silver, red, fuchsia, lime, yellow, blue, aqua, black
     colorRecArray = ["c0c0c0", "ff0000", "ff00ff", "00ff00", "ffff00", "0000ff", "00ffff", "000000"]
     if self.colorBarUnlink == True : # only build new colorBar if previous was unlinked
       self.colorBar = avg.DivNode( id="colors", parent=self.player.getRootNode() ) # container for colors
       self.colorBarUnlink = False
 
-    ''' fill the colorBar with the given colors '''
+    # fill the colorBar with the given colors
     countWidth = 0
     colorCount = 0
     
@@ -258,18 +276,36 @@ class Draw(object):
 
     
   def setColor ( self, event ):
-    ''' sets the self.color to the color of the chosen rectangle '''
+    '''
+    sets the self.color to the color of the chosen rectangle
+    '''
     self.color = event.node.fillcolor
     self.colorBar.unlink( )
     self.colorBarUnlink = True
+    self.updateChooserNode()
 
     self.sizeBar.unlink( )    # have to be deleted if outsourced to another icon
     self.sizeBarUnlink = True # have to be deleted if outsourced to another icon
+    
+  
+  def updateChooserNode(self):
+    '''
+    updates the chooser node to the set size and color
+    '''
+    if self.chooserNode.r != self.size:
+      # animatie the size change
+      LinearAnim(self.chooserNode, "r", 500, self.chooserNode.r, self.size).start()
+    if self.chooserNode.fillcolor != self.color:
+      self.chooserNode.fillcolor = self.color
+  
 
   def eraser(self, event):
     '''
     event handler function that selects eraser tool
     '''
+    # previous pencil color (to restore after erasing)
+    self.oldColor = self.color
+    
     self.color = "FFFFFF"
   
   
