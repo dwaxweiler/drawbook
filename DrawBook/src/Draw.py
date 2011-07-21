85c8a7a832425faaccf492fa63871df222f500ea
daniel.waxweiler <daniel.waxweiler@ba35e0a5-d65c-67f0-5c3e-c6f1f0da9a22>
2011-07-21 10:11:50 +0000

    icons are now vertically centered, changed background color of toolbar to a lighter grey

--- DrawBook/src/Draw.py
+++ DrawBook/src/Draw.py
@@ -4,7 +4,6 @@ DrawBook
 To do:
 - add possibility to change size
 - add possibility to take webcam photos
-- center toolbar icons vertically
 Bugs:
 - strange effects when user is drawing and moves with the finger pressed down right over the tool bar
 - crashes when touching/clicking pencil button twice
@@ -40,28 +39,31 @@ class Draw(object):
     # create a container for all the tool bar elements
     self.toolBar = avg.DivNode(id="tools", parent=player.getRootNode())
     # create the background of the tool bar
-    avg.RectNode(fillcolor="000000", fillopacity=1.0, parent=self.toolBar, pos=(0, 0),
+    avg.RectNode(fillcolor="2a2a2a", fillopacity=1.0, parent=self.toolBar, pos=(0, 0),
                  size=(screenWidth-imageWidth, screenHeight), strokewidth=0)
-    
+    # container that holds all the icons
+    icons = avg.DivNode(id="icons", parent=self.toolBar, pos=(5, 5))
     # pace the icons with their functionality on the tool bar
     self.n = screenWidth-imageWidth-10
     # webcam button
-    webcam = avg.ImageNode(href="img/webcam.png", parent=self.toolBar, pos=(5, 50), size=(self.n, self.n))
+    webcam = avg.ImageNode(href="img/webcam.png", parent=icons, pos=(0, 0), size=(self.n, self.n))
     webcam.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.webcam)
     # pencil button
-    tool = avg.ImageNode(href="img/pencil.png", parent=self.toolBar, pos=(5, 200), size=(self.n, self.n))
+    tool = avg.ImageNode(href="img/pencil.png", parent=icons, pos=(0, self.n), size=(self.n, self.n))
     self.tool = tool # for dynamic use of the pencil button position
     tool.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.pencil)
     # eraser button
-    eraser = avg.ImageNode(href="img/eraser.png", parent=self.toolBar, pos=(5, 350), size=(self.n, self.n))
+    eraser = avg.ImageNode(href="img/eraser.png", parent=icons, pos=(0, 2*self.n), size=(self.n, self.n))
     eraser.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.eraser)
     # save button
-    save = avg.ImageNode(href="img/apply.png", parent=self.toolBar, pos=(5, 500), size=(self.n, self.n))
+    save = avg.ImageNode(href="img/apply.png", parent=icons, pos=(0, 3*self.n), size=(self.n, self.n))
     save.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.save)
     # cancel button
-    cancel = avg.ImageNode(href="img/cancel.png", parent=self.toolBar, pos=(5, 650), size=(self.n, self.n))
+    cancel = avg.ImageNode(href="img/cancel.png", parent=icons, pos=(0, 4*self.n), size=(self.n, self.n))
     cancel.setEventHandler(avg.CURSORDOWN, avg.TOUCH|avg.MOUSE, self.cancel)
-
+    # center the icons vertically
+    icons.y = (self.screenHeight - 5*self.n) / 2
+    
     # create canvas for the drawing surface
     self.drawCanvas = player.loadCanvasString("<canvas id=\"drawing\" width=\""+str(imageWidth)+"\" height=\""+str(screenHeight)+"\"></canvas>")
     avg.RectNode(fillcolor="FFFFFF", fillopacity=1.0, parent=self.drawCanvas.getRootNode(),
